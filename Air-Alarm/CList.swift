2b77c1de28984e3201ce4bad6b05cf06b9bc11a7
parkjinhee53 <parkjh6275@naver.com>
2021-08-10 09:32:24 +0900

    디자인&새로고침

---
+++ Air-Alarm/CList.swift
@@ -0,0 +1,20 @@
+//
+//  CList.swift
+//  Air-Alarm
+//
+//  Created by Jinhee on 2021/08/10.
+//
+
+import SwiftUI
+
+struct CList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CList_Previews: PreviewProvider {
+    static var previews: some View {
+        CList()
+    }
+}
---
+++ Air-Alarm/MainTabView.swift
@@ -0,0 +1,78 @@
+//
+//  MainTabView.swift
+//  Air-Alarm
+//
+//  Created by Jinhee on 2021/08/10.
+//
+
+import SwiftUI
+
+enum AppMenu : String {
+    case AList = "Tab 1"
+    case BList = "Tab 2"
+    case CList = "Tab 3"
+}
+
+struct MainTabView: View {
+    
+    private enum Tabs {
+        case AList, BList, CList
+    }
+    
+    @State private var selectedTab: Tabs = .AList
+    @Binding var TitleOfNavi: String
+    
+    var body: some View {
+        return NavigationView{
+            ZStack {
+                TabView(selection: $selectedTab) {
+                    Group{
+                        AListUI
+                        BListUI
+                        CListUI
+                    }
+                }
+                .statusBar(hidden: selectedTab == .AList)
+                .statusBar(hidden: selectedTab == .BList)
+                .statusBar(hidden: selectedTab == .CList)
+            }
+
+        }
+    }
+}
+
+private extension MainTabView {
+    var AListUI: some View {
+        AListView() // Tab 1의 화면
+        .tag(1)
+        .tabItem {Label("Tab 1", systemImage: "1.square")}
+        .navigationBarHidden(false)
+        .onAppear {
+            self.TitleOfNavi = AppMenu.AList.rawValue
+        }
+    }
+    var BListUI: some View {
+        BList() // Tab 2의 화면
+        .tag(1)
+        .tabItem {Label("Tab 3", systemImage: "2.square")}
+        .navigationBarHidden(false)
+        .onAppear {
+            self.TitleOfNavi = AppMenu.BList.rawValue
+        }
+    }
+    var CListUI: some View {
+        CList() // Tab 3의 화면
+        .tag(1)
+        .tabItem {Label("Tab 3", systemImage: "3.square")}
+        .navigationBarHidden(false)
+        .onAppear {
+            self.TitleOfNavi = AppMenu.CList.rawValue
+        }
+    }
+}
+
+//struct MainTabView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        MainTabView()
+//    }
+//}
