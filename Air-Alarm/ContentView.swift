92cf2ac08b0ee4c306bb63b3187fbba4f005f9f2
Kim, GiBeom <gibum1228@naver.com>
2021-08-14 21:35:19 +0900

    rest api code refactoring

--- Air-Alarm/ContentView.swift
+++ Air-Alarm/ContentView.swift
@@ -8,58 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
-    struct Database: Codable {
-        var CO2: Double = 0.0
-        var dust: Double = 0.0
-        var humidity: Double = 0.0
-        var id: Int = 0
-        var temperature: Double = 0.0
-        var time: String = ""
-    }
-    @State var db = Database()
+    let restAPI = RestAPI()
+    @State var db = RestAPI.Database()
     
-    // 객체에 데이터 저장하기
-    func GET(){
-        if let url = URL(string: "http://mirsv.com:5000/get") {
-            var request = URLRequest.init(url: url)
-
-            request.httpMethod = "GET"
-
-            URLSession.shared.dataTask(with: request) { (data, response, error) in
-                guard let data = data else { return }
-
-                // data
-                let decoder = JSONDecoder()
-                if let json = try? decoder.decode(Database.self, from: data) {
-                    self.db = json
-                }
-            }.resume()
-        }
+    func update() {
+        self.db = restAPI.GET()
     }
-    
-    // 변수에 데이터 직접 저장하긴
-//    func GET1(){
-//        if let url = URL(string: "http://mirsv.com:5000/get") {
-//            var request = URLRequest.init(url: url)
-//
-//            request.httpMethod = "GET"
-//
-//            URLSession.shared.dataTask(with: request) { (data, response, error) in
-//                guard let data = data else { return }
-//
-//                // data
-//                if let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [String : Any] {
-//                    print("json에 데이터 들어감")
-//                    print(json["CO2"]!)
-//                    self.textView = json["CO2"]! as! Int
-//                }
-//            }.resume()
-//        }
-//    }
     
     var body: some View {
         VStack {
-            Button(action: GET){
+            Button(action: update){
                 Text("버튼")
             }
             Text("CO2: " + String(db.CO2))
