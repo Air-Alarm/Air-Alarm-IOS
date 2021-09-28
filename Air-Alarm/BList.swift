b49ce0589bfa553dd7db9ed7c3448edc75175064
parkjinhee53 <parkjh6275@naver.com>
2021-09-28 09:38:19 +0900

    일,주,월 누르면 화면 바뀜

--- Air-Alarm/BList.swift
+++ Air-Alarm/BList.swift
@@ -10,32 +10,66 @@ import SwiftUICharts
 
 struct BList: View {
     @State var pickerSelectedItem = 0
+    @State var tag:Int? = nil
+    private var pickerArray = ["일", "주", "월"]
+    
     var body: some View {
-        VStack {
-            ScrollView {
-//            Picker(selection: $pickerSelectedItem, label: Text("")) {
-//                Text("온도").tag(0)
-//                Text("습도").tag(1)
-//                Text("미세먼지").tag(2)
-//            }.pickerStyle(SegmentedPickerStyle())
-//            .padding(.horizontal, 24)
-            
-                LineView(data: Data.data0, title: Data.title, legend: Data.legend0, style: Style.metoring)
-                    .padding()
-                    .frame(width: 360, height: 365)
-                LineView(data: Data.data1, title: Data.title, legend: Data.legend1, style: Style.metoring)
-                    .padding()
-                    .frame(width: 360, height: 365)
-                LineView(data: Data.data2, title: Data.title, legend: Data.legend2, style: Style.metoring)
-                    .padding()
-                    .frame(width: 360, height: 365)
-                LineView(data: Data.data3, title: Data.title, legend: Data.legend3, style: Style.metoring)
-                    .padding()
-                    .frame(width: 360, height: 365)
+        NavigationView {
+            VStack {
+//                Picker(selection: $pickerSelectedItem, label: Text("")) {
+//                        Text("일")
+//                            .tag(0)
+//                        Text("주")
+//                            .tag(1)
+//                        Text("월")
+//                            .tag(2)
+//                }.pickerStyle(SegmentedPickerStyle())
+//                .padding(.horizontal, 24)
+                
+                HStack {
+                    NavigationLink(destination: BList()) {
+                        Text("일")
+                            .padding(.horizontal, 30.0)
+                            .foregroundColor(.red)
+                    }
+                    NavigationLink(destination: Charts1()) {
+                        Text("주")
+                            .padding(.horizontal, 30.0)
+                    }
+                    NavigationLink(destination: Charts2()) {
+                        Text("월")
+                            .padding(.horizontal, 30.0)
+                    }
+                }
+                .padding(.horizontal,10.0)
+                Divider()
                 Spacer()
+                VStack {
+                    ScrollView {
+                        LineView(data: Data.data0, title: Data.title, legend: Data.legend0, style: Style.metoring)
+                            .padding()
+                            .frame(width: 360, height: 365)
+                            .tag(0)
+                        LineView(data: Data.data1, title: Data.title, legend: Data.legend1, style: Style.metoring)
+                            .padding()
+                            .frame(width: 360, height: 365)
+                            .tag(0)
+                        LineView(data: Data.data2, title: Data.title, legend: Data.legend2, style: Style.metoring)
+                            .padding()
+                            .frame(width: 360, height: 365)
+                            .tag(0)
+                        LineView(data: Data.data3, title: Data.title, legend: Data.legend3, style: Style.metoring)
+                            .padding()
+                            .frame(width: 360, height: 365)
+                            .tag(0)
+                        Spacer()
+                    }
+                    .padding()
+                }
             }
-            .padding()
         }
+        .navigationBarHidden(true)
+        .navigationBarBackButtonHidden(true)
     }
 }
 struct BList_Previews: PreviewProvider {
