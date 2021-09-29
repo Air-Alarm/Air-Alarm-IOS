0adceb45dcbdd59337c167cab881440a1104cd88
Kim haeun <haeunkim0807@naver.com>
2021-09-29 21:32:15 +0900

    merge작업중

---
+++ Air-Alarm/DTO/Login.swift
@@ -0,0 +1,20 @@
+//
+//  Login.swift
+//  Air-Alarm
+//
+//  Created by 김하은 on 2021/09/29.
+//
+
+import SwiftUI
+
+struct Login: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Login_Previews: PreviewProvider {
+    static var previews: some View {
+        Login()
+    }
+}
